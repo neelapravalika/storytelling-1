191720c68096700b3f4e3528c710445ed83e2828
neelapravalika <54702274+neelapravalika@users.noreply.github.com>
2021-05-26 12:26:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import DrawerNavigator from "./navigation/DrawerNavigator";
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <DrawerNavigator />
+    </NavigationContainer>
+  );
+}
+
+/*const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});*/
